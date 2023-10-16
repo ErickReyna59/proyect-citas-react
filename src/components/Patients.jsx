4b25295e0cbd43079cd62123560da2344ed39729
ErickReyna59 <130819788+ErickReyna59@users.noreply.github.com>
2023-10-16 13:59:23 -0600

    feat: update of each record

--- src/components/Patients.jsx
+++ src/components/Patients.jsx
@@ -1,32 +1,24 @@
-function Patients() {
+function Patients({ patients }) {
+  const { name, owner, email, high, symptoms } = patients;
   return (
     <div className="mx-4 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Name: <span className="font-normal normal-case">Hook</span>
+        Name: <span className="font-normal normal-case">{name}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Owner: <span className="font-normal normal-case">Hook</span>
+        Owner: <span className="font-normal normal-case">{owner}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Email:{" "}
-        <span className="font-normal normal-case">correo@correo.com</span>
+        Email: <span className="font-normal normal-case">{email}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        High: <span className="font-normal normal-case">10 Friday 2020</span>
+        High: <span className="font-normal normal-case">{high}</span>
       </p>
       <p className="font-bold mb-3 text-gray-700 uppercase">
-        Symptoms:{" "}
-        <span className="font-normal normal-case">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum itaque
-          facilis animi similique adipisci! Laborum voluptatum quam repellendus,
-          voluptatibus illum voluptate consectetur voluptates quae debitis
-          soluta, reiciendis error! Porro, sequi?
-        </span>
+        Symptoms: <span className="font-normal normal-case">{symptoms}</span>
       </p>
     </div>
   );
 }
 
 export default Patients;
-
-
