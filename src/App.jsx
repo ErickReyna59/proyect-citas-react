26973a489f9fa7f029959ee46222d42cca103744
ErickReyna59 <130819788+ErickReyna59@users.noreply.github.com>
2023-10-16 09:33:25 -0600

    feat: added the functions to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import React from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import ListPatients from "./components/ListPatients";
 
 function App() {
+  const [patients, setPatients] = useState([]);
+
   return (
     <div className="container mt-20 mx-auto">
       <Header />
       <div className="mt-12 md:flex">
-        <Form />
+        <Form patients={patients} setPatients={setPatients} />
         <ListPatients />
       </div>
     </div>
