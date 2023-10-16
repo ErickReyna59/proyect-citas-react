917b813cd5642e976e97d7e325f4ad4f394ae75c
ErickReyna59 <130819788+ErickReyna59@users.noreply.github.com>
2023-10-16 12:46:16 -0600

    feat: add a children to component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Error = ({ message }) => {
+const Error = ({ children }) => {
   return (
     <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md ">
       {" "}
-      <p>{message}</p>
+      <p>{children}</p>
     </div>
   );
 };
